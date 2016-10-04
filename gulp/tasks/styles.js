e04f003b7fdba076664a96f55b555e6cc233467d
Ann Cascarano <a.cascarano@gmail.com>
2016-10-04 19:53:36 -0400

    added mixin postcss package and more styling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
   /* the 'return' is necessary to respect the asynchronous nature of postcss*/
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
   .on('error', function(errorDescription){
     console.log(errorDescription.toString());
     this.emit('end');
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -12,7 +12,6 @@ gulp.task('watch', function(){
     });
     
     watch('./app/index.html', function(){
-      gulp.start('html');
       browserSync.reload();
     });
     
