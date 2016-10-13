130174ec196d5ad5b611ec6265d0a29dde0ca0fd
Ann Cascarano <a.cascarano@gmail.com>
2016-10-13 09:53:55 -0400

    setting up for modern JS with Babel

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,18 @@
 var $ = require('jquery');
-var Person = require('./modules/Person.js');
+//var Person = require('./modules/Person.js');
+import Person from './modules/Person';
 
-var john = new Person("John Doe", "blue");
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes 0$.");
+  }
+}
+
+var john = new Person("John Doe", "orange");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "green");
 jane.greet();
+jane.payTaxes();
 
 // $("h1").hide();
