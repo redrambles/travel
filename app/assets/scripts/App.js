7f297d221005bdcce2dad930727b87124bfb6324
Ann Cascarano <a.cascarano@gmail.com>
2016-10-13 15:04:29 -0400

    working mobile menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person.js');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes 0$.");
-  }
-}
-
-var john = new Person("John Doe", "orange");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
-
-// $("h1").hide();
+var mobileMenu = new MobileMenu();
