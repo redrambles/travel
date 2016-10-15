77df4efab8c0fa4f2964e0430f2545e0139c2d3f
Ann Cascarano <a.cascarano@gmail.com>
2016-10-15 15:33:53 -0400

    add reveal on scroll functionality
    for testimonials and featured items sections.

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll{
+  constructor(element, offset){
+    this.itemsToReveal = element;
+    this.offsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+  
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
+  
+  createWaypoints(){
+    var that = this; // so that the 'this' keyword still points to the class and not the new 'Waypoint' object created below  
+    this.itemsToReveal.each(function(){
+      var currentItem = this;
+        new Waypoint({
+          element: currentItem,
+          handler: function(){
+            $(currentItem).addClass("reveal-item--is-visible");
+          },
+          offset: that.offsetPercentage
+        });
+    });
+  }
+  
+}
+
+export default RevealOnScroll;
